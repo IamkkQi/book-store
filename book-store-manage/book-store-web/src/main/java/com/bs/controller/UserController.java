bf9f9373b91404d2fd6d0dd8010e131c7a78ddf0
IamkkQi <IamkkQi@163.com>
2017-02-22 18:08:01 +0800

    环境搭建完成

---
+++ book-store-manage/book-store-web/src/main/java/com/bs/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.bs.controller;
+
+import com.bs.pojo.User;
+import com.bs.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by qkk on 2017/2/22.
+ */
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    
+    @Resource
+    private UserService userService;
+
+    @RequestMapping("test")
+    public String test() {
+        List<User> users = userService.listAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        return "index";
+    }
+}
