fbf3cbe1dc35a9fdb9b39ecc007986d82645ce76
Kay <iamkkqi@163.com>
2017-04-11 23:53:29 +0800

    引入后台子项目

---
+++ book-store-manage/book-store-admin/src/main/java/com/backstage/controller/AdminController.java
@@ -0,0 +1,11 @@
+package com.backstage.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Kay on 2017/4/11.
+ */
+
+@Controller
+public class AdminController {
+}
