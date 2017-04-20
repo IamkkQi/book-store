9f156d0bf422182a44220a8e94fe726664fd1ac6
IamkkQi <IamkkQi@163.com>
2017-04-20 19:10:57 +0800

    菜单更新

--- book-store-manage/book-store-admin/src/main/java/com/backstage/controller/UserController.java
+++ book-store-manage/book-store-admin/src/main/java/com/backstage/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.backstage.controller;
 
+import com.bs.bean.PageBean;
+import com.bs.constants.Constants;
 import com.bs.pojo.User;
 import com.bs.service.UserService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +15,17 @@ import java.util.List;
 /**
  * Created by Kay on 2017/4/13.
  */
-@RequestMapping("admin/user")
+@Controller
+@RequestMapping("/admin/user")
 public class UserController {
     @Resource
     private UserService userService;
 
-    @RequestMapping("list")
+    @RequestMapping("/list")
     public String list(Model model, @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
         List<User> users = userService.listAllUsers(pageNum);
-
+        PageBean<User> pageBean = new PageBean<>(pageNum, Constants.PAGE_SIZE, users, userService.countAllUsers());
+        model.addAttribute("pageBean", pageBean);
         return "user/list";
     }
 }
